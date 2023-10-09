0c5fe4746a2421167b46a2d428c35bc8820f5536
JRSkates <skatesproduction@gmail.com>
2023-10-09 17:44:11 +0100

    remove duplicates function

---
+++ remove_duplicates.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def find_duplicates(nums: List[int]) -> List[int]:
+    seen = set()
+    duplicates = set()
+    
+    for num in nums:
+        if num in seen:
+            duplicates.add(num)
+        else:
+            seen.add(num)
+    
+    return list(duplicates)
