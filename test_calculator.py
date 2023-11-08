12712802e08d0dfb2b6f84f55679d349558819de
JRSkates <skatesproduction@gmail.com>
2023-11-08 17:18:33 +0000

    subtract function created

--- test_calculator.py
+++ test_calculator.py
@@ -4,3 +4,8 @@ def test_addition():
   calc = Calculator()
   result = calc.add(3, 5)
   assert result == 8
+
+def test_subtraction():
+  calc = Calculator()
+  result = calc.subtract(5, 3)
+  assert result == 2
