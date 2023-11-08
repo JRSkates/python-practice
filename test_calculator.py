01845359661492af8808bcc443c8a622a0474908
JRSkates <skatesproduction@gmail.com>
2023-11-08 17:19:17 +0000

    multiply function created

--- test_calculator.py
+++ test_calculator.py
@@ -9,3 +9,8 @@ def test_subtraction():
   calc = Calculator()
   result = calc.subtract(5, 3)
   assert result == 2
+
+def test_multiply():
+  calc = Calculator()
+  result = calc.multiply(3, 5)
+  assert result == 15
