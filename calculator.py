220795b2e8b25c4377bd1ed7d533e3df6f75d476
JRSkates <skatesproduction@gmail.com>
2023-11-14 19:52:30 +0000

    __init__ added to Class

--- calculator.py
+++ calculator.py
@@ -1,18 +1,28 @@
 class Calculator:
+  def __init__(self):
+    self.result = 0
+    self.memory = 0
+
   def add(self, x, y):
-    return x + y
+    self.result = x + y
+    return self.result
   
   def subtract(self, x, y):
-    return x - y
+    self.result = x - y
+    return self.result
   
   def multiply(self, x, y):
-    return x * y
+    self.result = x * y
+    return self.result
   
   def divide(self, x, y):
-    return x / y
+    self.result = x / y
+    return self.result
 
   def raise_to_power_of(self, x, y):
-    return x**y
+    self.result = x**y
+    return self.result
   
   def remainder(self, x, y):
-    return x % y
+    self.result = x % y
+    return self.result
